0d0ce53fe432104c148e90b76e8c316228b35524
soup-bowl <code@soupbowl.io>
2024-05-26 23:52:53 +0100

    Mobile view table.

--- frontend/src/Pages/Review.tsx
+++ frontend/src/Pages/Review.tsx
@@ -3,10 +3,14 @@ import { ScrollArea } from "@/Components/ui/scroll-area"
 import { useLocalStorage, useLocalStorageSingle } from "@/Hooks"
 import { NameStorage } from "@/Types"
 import { Button } from "@/Components/ui/button"
+import { useEffect, useState } from "react"
 
 function Review() {
 	const [records, setRecords] = useLocalStorage<NameStorage[]>("eggsalad-choices", [])
+	const [recordsSorted, setRecordsSorted] = useState<NameStorage[]>([])
 	const [surname] = useLocalStorageSingle("eggsalad-surname", "Smith")
+
+	useEffect(() => setRecordsSorted(records.sort(ReviewSort)), [records])
 
 	const RemoveChoice = (name: NameStorage) => {
 		setRecords((names) => {
@@ -31,36 +35,62 @@ function Review() {
 	return (
 		<>
 			<ScrollArea className="h-[400px] w-full">
-				<Table className="text-black">
-					<TableHeader>
-						<TableRow>
-							<TableHead>Name</TableHead>
-							<TableHead>Gender</TableHead>
-							<TableHead>Decision</TableHead>
-							<TableHead className="w-[120px]">Actions</TableHead>
-						</TableRow>
-					</TableHeader>
-					<TableBody className="text-xl">
-						{records.sort(ReviewSort).map((name) => {
-							const chosenColour = name.Accepted ? "bg-green-100" : "bg-red-100"
+				<div className="block sm:hidden">
+					<Table className="text-black">
+						<TableBody className="text-xl">
+							{recordsSorted.map((name) => {
+								const chosenColour = name.Accepted ? "bg-green-100" : "bg-red-100"
 
-							return (
-								<TableRow className={chosenColour} key={name.Name}>
-									<TableCell className="font-medium">
-										{name.Name}&nbsp;{surname}
-									</TableCell>
-									<TableCell>{name.Gender}</TableCell>
-									<TableCell>{name.Accepted ? <>✔️</> : <>❌</>}</TableCell>
-									<TableCell>
-										<Button size="sm" onClick={() => RemoveChoice(name)}>
-											Delete
-										</Button>
-									</TableCell>
-								</TableRow>
-							)
-						})}
-					</TableBody>
-				</Table>
+								return (
+									<TableRow className={chosenColour} key={name.Name}>
+										<TableCell className="text-left">
+											<p className="font-medium">{name.Name}&nbsp;{surname}</p>
+											<p className="text-sm">{name.Gender}</p>
+										</TableCell>
+										<TableCell>
+											<Button size="icon" onClick={() => RemoveChoice(name)}>
+												X
+											</Button>
+										</TableCell>
+									</TableRow>
+								)
+							})}
+						</TableBody>
+					</Table>
+				</div>
+	
+				<div className="hidden sm:block">
+					<Table className="text-black">
+						<TableHeader>
+							<TableRow>
+								<TableHead>Name</TableHead>
+								<TableHead>Gender</TableHead>
+								<TableHead>Decision</TableHead>
+								<TableHead className="w-[120px]">Actions</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody className="text-xl">
+							{recordsSorted.map((name) => {
+								const chosenColour = name.Accepted ? "bg-green-100" : "bg-red-100"
+
+								return (
+									<TableRow className={chosenColour} key={name.Name}>
+										<TableCell className="font-medium">
+											{name.Name}&nbsp;{surname}
+										</TableCell>
+										<TableCell>{name.Gender}</TableCell>
+										<TableCell>{name.Accepted ? <>✔️</> : <>❌</>}</TableCell>
+										<TableCell>
+											<Button size="sm" onClick={() => RemoveChoice(name)}>
+												Delete
+											</Button>
+										</TableCell>
+									</TableRow>
+								)
+							})}
+						</TableBody>
+					</Table>
+				</div>
 			</ScrollArea>
 		</>
 	)
