4ea1de3638c97c43b06ff6c1f55384b82e1ed254
soup-bowl <code@soupbowl.io>
2024-05-28 21:38:57 +0000

    Added warning.

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -2,6 +2,7 @@ import { createContext } from "react"
 import Selection from "@/Pages/Selection"
 import { useNameData } from "@/Hooks"
 import { NameRecords } from "@/Types"
+import { TriangleAlert } from "lucide-react"
 
 export const DataContext = createContext<NameRecords[]>([])
 
@@ -15,6 +16,10 @@ function App() {
 	return (
 		<DataContext.Provider value={data.data}>
 			<div className="flex flex-col gap-4 items-center justify-center min-h-screen bg-brutal3">
+				<p className="bg-yellow-300 text-black border-2 border-black shadow-brutal-drop-md p-4">
+					<TriangleAlert className="inline mr-2" />
+					This is a <strong>beta application</strong> - data loss may occur
+				</p>
 				<Selection />
 				<p className="text-gray-800">
 					made by{" "}
