1767dae96bb6947f4ac1d2aa429c8b9d39c97392
soup-bowl <code@soupbowl.io>
2024-06-09 20:18:06 +0100

    Cache the name data locally.

--- src/Hooks/Names.ts
+++ src/Hooks/Names.ts
@@ -3,6 +3,7 @@ import { NameRecords } from "@/Types"
 import { parseNameData } from "@/Utils"
 
 const NAME_SOURCE = import.meta.env.VITE_NAME_SOURCE
+const CACHE_KEY = "eggsalad-namecache"
 
 const fetchData = async (): Promise<string> => {
 	const response = await fetch(NAME_SOURCE)
@@ -18,13 +19,27 @@ const useNameData = () => {
 
 	useEffect(() => {
 		const loadData = async () => {
+			let cachedData: string | null = null
+
 			try {
 				setLoading(true)
+
+				cachedData = localStorage.getItem(CACHE_KEY)
+				if (cachedData) {
+					const parsedCachedData = JSON.parse(cachedData)
+					setData(parsedCachedData)
+					setLoading(false)
+				}
+
 				const csvData = await fetchData()
 				const parsedData = parseNameData(csvData)
 				setData(parsedData)
+
+				localStorage.setItem(CACHE_KEY, JSON.stringify(parsedData))
 			} catch (err) {
-				setError(err as Error)
+				if (!cachedData) {
+					setError(err as Error)
+				}
 			} finally {
 				setLoading(false)
 			}
